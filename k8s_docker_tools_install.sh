424636a65b636e5293b21a4a17cc40afc9ec8268
Kishore Kumar <mcalbert@gmail.com>
2022-12-18 18:37:04 +0530

    Update script

--- k8s_docker_tools_install.sh
+++ k8s_docker_tools_install.sh
@@ -35,6 +35,12 @@ BACKTITLE="Minikube or Kind installation"
 TITLE="Local Kubernetes Cluster Installation"
 MENU="Choose one of the following:"
 
+if ! [ -x "$(command -v dialog)" ]; then
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: dialog is not installed. Hence installing\n"
+  sudo apt install dialog
+fi
+
 OPTIONS=(1 "Minikube"
          2 "Kind")
 
@@ -49,15 +55,27 @@ CHOICE=$(dialog --clear \
 
 case $CHOICE in
         1)
-            curl -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
-            sudo install minikube-linux-amd64 /usr/local/bin/minikube
+            echo -e "\n--------------------------------------------------------------\n"
+            if ! [ -x "$(command -v minikube)" ]; then
+              curl -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+              sudo install minikube-linux-amd64 /usr/local/bin/minikube
+            else
+              echo -e "\nMinikube already installed!!\n"
+            fi
             ;;
         2)
-            curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64"
-            sudo mv ./kind /bin/kind
+            echo -e "\n--------------------------------------------------------------\n"
+            if ! [ -x "$(command -v kind)" ]; then
+              curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64"
+              chmod +x ./kind
+              sudo install ./kind /usr/local/bin/kind
+            else
+              echo -e "\nKind already installed!!\n"
+            fi
             ;;
         *)
-            echo "Exiting!"
+            echo -e "\n--------------------------------------------------------------\n"
+            echo -e "\nExiting!\n"
 esac
 }
 check_if_installed(){
@@ -67,18 +85,22 @@ cd $INSTALL_DIR
 # Check required commands are available and executable in $PATH
 
 if ! [ -x "$(command -v docker)" ]; then
-  echo 'Error: docker is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: docker is not installed. Hence installing\n"
   sudo apt update
   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
   sudo apt update
   sudo apt install -y docker-ce
+  sudo usermod -aG docker $USER
+  sudo chmod 666 /var/run/docker.sock
   sudo systemctl status docker
 fi
 
 if ! [ -x "$(command -v crictl)" ]; then
-  echo -e 'Error: crictl is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: crictl is not installed. Hence installing\n"
   VERSION="v1.24.1"
   curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-${VERSION}-linux-amd64.tar.gz --output crictl-${VERSION}-linux-amd64.tar.gz
   sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
@@ -86,41 +108,48 @@ if ! [ -x "$(command -v crictl)" ]; then
 fi
 
 if ! [ -x "$(command -v kubectl)" ]; then
-  echo -e 'Error: kubectl is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: kubectl is not installed. Hence installing\n"
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   sudo install kubectl /usr/local/bin/kubectl
 fi
 
 if ! [ -x "$(command -v helm)" ]; then
-  echo -e 'Error: helm is not installed'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: helm is not installed\n"
   #exit 1
 fi
 
 dialog_box_for_k8s
+sleep 10
 check_kubeconfig
 
 if ! [ -x "$(command -v k9s)" ]; then
-  echo "k9s is not installed. Hence installing in ${INSTALL_DIR}"
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nk9s is not installed. Hence installing in ${INSTALL_DIR}\n"
   curl -Lo k9s.tgz https://github.com/derailed/k9s/releases/download/v0.26.3/k9s_Linux_x86_64.tar.gz
   tar -xf k9s.tgz
   sudo install k9s /usr/local/bin/
 fi
 
 if ! [ -x "$(command -v kubectx)" ]; then
-  echo 'kubectx is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nkubectx is not installed. Hence installing\n"
   curl -Lo kubectx.tgz  https://github.com/ahmetb/kubectx/releases/download/v0.9.0/kubectx_v0.9.0_linux_x86_64.tar.gz
   tar -xf kubectx.tgz
   sudo install kubectx /usr/local/bin/
 fi
 
 if ! [ -x "$(command -v kubens)" ]; then
-  echo 'kubens is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nkubens is not installed. Hence installing\n"
   curl -Lo kubens.tgz  https://github.com/ahmetb/kubectx/releases/download/v0.9.0/kubens_v0.9.0_linux_x86_64.tar.gz 
   tar -xf kubens.tgz
   sudo install kubens /usr/local/bin/
 fi
 if ! [ -x "$(command -v fzf)" ]; then
-  echo 'fzf is not installed. Hence installing'
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nfzf is not installed. Hence installing\n"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 fi
