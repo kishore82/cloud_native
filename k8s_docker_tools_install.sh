32182428533fdcb747c6424acbf59ef920b0b5e4
kishore82 <36041580+kishore82@users.noreply.github.com>
2022-12-20 14:36:27 +0530

    Update k8s_docker_tools_install.sh

--- k8s_docker_tools_install.sh
+++ k8s_docker_tools_install.sh
@@ -15,7 +15,7 @@ then
 		echo -e "curl $KUBE_API/version --cacert $CERT_DIR/cacert.pem --key $CERT_DIR/key.pem --cert $CERT_DIR/cert.pem"
 	fi
 else
-	echo -e "\nCheck if KUBECONFIG is configured in ~/.bashrc. Do printenv | grep KUBECONFIG"
+	echo -e "\nError: Check if KUBECONFIG is configured in ~/.bashrc. Do printenv | grep KUBECONFIG"
 fi
 }
 make_or_remove_dir(){
@@ -57,20 +57,24 @@ case $CHOICE in
         1)
             echo -e "\n--------------------------------------------------------------\n"
             if ! [ -x "$(command -v minikube)" ]; then
+              echo -e "\n--------------------------------------------------------------\n"
+              echo -e "\nError: minikube is not installed. Hence installing\n"
               curl -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
               sudo install minikube-linux-amd64 /usr/local/bin/minikube
             else
-              echo -e "\nMinikube already installed!!\n"
+              echo -e "\nError: Minikube already installed!!\n"
             fi
             ;;
         2)
             echo -e "\n--------------------------------------------------------------\n"
             if ! [ -x "$(command -v kind)" ]; then
+              echo -e "\n--------------------------------------------------------------\n"
+              echo -e "\nError: kind is not installed. Hence installing\n"
               curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64"
               chmod +x ./kind
               sudo install ./kind /usr/local/bin/kind
             else
-              echo -e "\nKind already installed!!\n"
+              echo -e "\nError: Kind already installed!!\n"
             fi
             ;;
         *)
@@ -83,6 +87,13 @@ check_if_installed(){
 make_or_remove_dir $INSTALL_DIR
 cd $INSTALL_DIR
 # Check required commands are available and executable in $PATH
+
+if ! [ -x "$(command -v xclip)" ]; then
+  echo -e "\n--------------------------------------------------------------\n"
+  echo -e "\nError: xclip is not installed. Hence installing\n"
+  sudo apt update
+  sudo apt install xclip
+fi
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
@@ -122,27 +133,30 @@ if ! [ -x "$(command -v helm)" ]; then
 fi
 
 dialog_box_for_k8s
-sleep 10
 check_kubeconfig
 
 if [ -x "$(command -v git)" ]; then
-  echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nkrew is not installed. Hence installing\n"
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  sudo ./"${KREW}" install krew
+  if test -f ""; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: Krew is already installed\n"
+  else
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: krew is not installed. Hence installing\n"
+    OS="$(uname | tr '[:upper:]' '[:lower:]')"
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+    KREW="krew-${OS}_${ARCH}"
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz"
+    tar zxvf "${KREW}.tar.gz"
+    sudo ./"${KREW}" install krew
+  fi
 else
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nkrew is not installed. Please install Git first!!!\n"
+  echo -e "\n Error: krew is not installed. Please install Git first!!!\n"
 fi
 
 if ! [ -x "$(command -v k9s)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nk9s is not installed. Hence installing in ${INSTALL_DIR}\n"
+  echo -e "\n Error: k9s is not installed. Hence installing\n"
   curl -Lo k9s.tgz https://github.com/derailed/k9s/releases/download/v0.26.3/k9s_Linux_x86_64.tar.gz
   tar -xf k9s.tgz
   sudo install k9s /usr/local/bin/
@@ -150,7 +164,7 @@ fi
 
 if ! [ -x "$(command -v kubectx)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nkubectx is not installed. Hence installing\n"
+  echo -e "\n Error: kubectx is not installed. Hence installing\n"
   curl -Lo kubectx.tgz  https://github.com/ahmetb/kubectx/releases/download/v0.9.0/kubectx_v0.9.0_linux_x86_64.tar.gz
   tar -xf kubectx.tgz
   sudo install kubectx /usr/local/bin/
@@ -158,14 +172,14 @@ fi
 
 if ! [ -x "$(command -v kubens)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nkubens is not installed. Hence installing\n"
+  echo -e "\n Error: kubens is not installed. Hence installing\n"
   curl -Lo kubens.tgz  https://github.com/ahmetb/kubectx/releases/download/v0.9.0/kubens_v0.9.0_linux_x86_64.tar.gz 
   tar -xf kubens.tgz
   sudo install kubens /usr/local/bin/
 fi
 if ! [ -x "$(command -v fzf)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nfzf is not installed. Hence installing\n"
+  echo -e "\n Error: fzf is not installed. Hence installing\n"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 fi
