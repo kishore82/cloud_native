09fe0b5db12e75f81621257eddcb0ba86ce92029
kishore82 <36041580+kishore82@users.noreply.github.com>
2022-12-21 10:58:19 +0530

    Update k8s_docker_tools_install.sh

--- k8s_docker_tools_install.sh
+++ k8s_docker_tools_install.sh
@@ -15,7 +15,7 @@ then
 		echo -e "curl $KUBE_API/version --cacert $CERT_DIR/cacert.pem --key $CERT_DIR/key.pem --cert $CERT_DIR/cert.pem"
 	fi
 else
-	echo -e "\nError: Check if KUBECONFIG is configured in ~/.bashrc. Do printenv | grep KUBECONFIG"
+	echo -e "\nError: Check if \033[4mKUBECONFIG\033[0m is configured in ~/.bashrc. Do printenv | grep KUBECONFIG"
 fi
 }
 make_or_remove_dir(){
@@ -62,7 +62,7 @@ case $CHOICE in
               curl -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
               sudo install minikube-linux-amd64 /usr/local/bin/minikube
             else
-              echo -e "\nError: Minikube already installed!!\n"
+              echo -e "\nError: \033[4mMinikube\033[0m already installed!!\n"
             fi
             ;;
         2)
@@ -74,12 +74,9 @@ case $CHOICE in
               chmod +x ./kind
               sudo install ./kind /usr/local/bin/kind
             else
-              echo -e "\nError: Kind already installed!!\n"
+              echo -e "\nError: \033[4mKind\033[0m already installed!!\n"
             fi
             ;;
-        *)
-            echo -e "\n--------------------------------------------------------------\n"
-            echo -e "\nExiting!\n"
 esac
 }
 check_if_installed(){
@@ -126,7 +123,7 @@ fi
 
 if ! [ -x "$(command -v helm)" ]; then
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\nError: helm is not installed\n"
+  echo -e "\nError: helm is not installed. Hence installing\n"
   curl -Lo helm.tgz "https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz"
   tar -xf helm.tgz
   sudo install linux-amd64/helm /usr/local/bin/helm
@@ -136,10 +133,7 @@ dialog_box_for_k8s
 check_kubeconfig
 
 if [ -x "$(command -v git)" ]; then
-  if test -f "$HOME/.krew/bin/kubectl-krew"; then
-    echo -e "\n--------------------------------------------------------------\n"
-    echo -e "\nError: Krew is already installed\n"
-  else
+  if ! test -f "$HOME/.krew/bin/kubectl-krew"; then
     echo -e "\n--------------------------------------------------------------\n"
     echo -e "\nError: krew is not installed. Hence installing\n"
     OS="$(uname | tr '[:upper:]' '[:lower:]')"
@@ -151,7 +145,74 @@ if [ -x "$(command -v git)" ]; then
   fi
 else
   echo -e "\n--------------------------------------------------------------\n"
-  echo -e "\n Error: krew is not installed. Please install Git first!!!\n"
+  echo -e "\nError: krew is not installed. Please install \033[4mGit\033[0m first!!!\n"
+fi
+KREW_FILE="$HOME/.krew/bin/"
+if test -f "$HOME/.krew/bin/kubectl-krew"; then
+  if ! test -f "$HOME/.krew/bin/kubectl-cert_manager"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: cert-manager plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install cert-manager
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-graph"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: graph plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install graph
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-df_pv"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: df-pv plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install df-pv
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-oomd"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: oomd plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install oomd
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-cost"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: cost plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install cost
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-pod_dive"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: pod-dive plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install pod-dive
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-pod_lens"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: pod-lens plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install pod-lens
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-rbac_lookup"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: rbac-lookup plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install rbac-lookup
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-rbac_tool"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: rbac-tool plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install rbac-tool
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-ttsum"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: ttsum plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install ttsum
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-view_cert"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: view-cert plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install view-cert
+  fi
+  if ! test -f "$HOME/.krew/bin/kubectl-view_secret"; then
+    echo -e "\n--------------------------------------------------------------\n"
+    echo -e "\nError: view-secret plugin is not installed. Hence installing\n"
+    $KREW_FILE/kubectl-krew install view-secret
+  fi
+fi
+ans=$($KREW_FILE/kubectl-krew list | awk '{print $1}' | wc -l)
+if [ $ans == 13 ] ; then
+  echo -e "\n\033[4m13\033[0m krew plugins are already installed. Please do '\033[4mkubectl krew list\033[0m' to check\n"
 fi
 
 if ! [ -x "$(command -v k9s)" ]; then
